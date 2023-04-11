6556c48a678d6b03c824cb728924810650b06ff9
foxion <foxion37@gmail.com>
2023-04-11 16:35:13 +0900

    message

---
+++ models/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Post
+from .forms import PostForm
+
+def post_create(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save()
+            return redirect('post_detail', pk=post.pk)
+        else :
+            form = PostForm()  
+        return render(request, 'post_detail.html',{"post":post})
+def post_list(request):
+    post = post.object.all()
+    return render(request, 'post_list.html',{"post":post})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'post_detail.html',{"post":post})
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == 'POST':
+       form = PostForm(request.POST, instance = post )
+       if form.is_valid():
+           post=form.save()
+           return redirect('post_detail', pk=post.pk)
+    else :
+        form = PostForm(instance=post)
+    return render(request, 'post_edit.html', {'form':form})
+
+def post_delete(request, pk):
+    post = get_object_or_404(post, pk=pk)
+    post.delete()
+    return redirect('post_list')
+
+
+
+
+
+# Create your views here.
